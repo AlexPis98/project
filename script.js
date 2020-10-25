c75ffb8fe583e3f652414d6ed17b98dcd8582954
Sasha <alexandr-pisarev98@mail.ru>
2020-10-25 12:24:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+/*"use strict";
+let number = 15; // - тип данных число
+
+
+const presone = "Alex"; 
+const stroka = "5"; 
+const bool = true;
+
+console.log(bek); // null
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "Sasha",
+    age: 25,
+    IsMarried: false
+}
+
+console.log(obj.IsMarried);
+
+let arr = ['plum.png', 'orange.jpg', 6 , 'apple.bmp'];
+console.log(arr[1]);*/
+
+//alert('Hello');
+
+//const result = confirm("Are you here");
+//console.log(result);
+
+/* const answer = prompt("are u got 18?", "18");
+console.log(typeof(answer)); */
+
+// const answers = [];
+
+// answers[0] = prompt('how u','');
+// answers[1] = prompt('how ua','');
+// answers[2] = prompt('how uds','');
+
+// document.write(answers);
+
+// const category = 'toys';
+// console.log('https://someurl.come/');
+
+const user = 'name';
+alert(`hie, ${user}`);
