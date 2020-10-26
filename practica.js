30f60c152b8afcd2cb799d11f7e72561a0f985be
Sasha <alexandr-pisarev98@mail.ru>
2020-10-26 22:07:19 +0300

    second

--- practica.js
+++ practica.js
@@ -1,20 +1,43 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
 
-const presonalMovieDB = {
-count: numberOfFilms,
-movies: {},
-actros: {},
-genres: [],
-privat: false
+// const presonalMovieDB = {
+// count: numberOfFilms,
+// movies: {},
+// actros: {},
+// genres: [],
+// privat: false
 
-};
+// };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его', '');
+// const a = prompt('Один из последних просмотренных фильмов', ''),
+//       b = prompt('На сколько оцените его', ''),
+//       c = prompt('Один из последних просмотренных фильмов', ''),
+//       d = prompt('На сколько оцените его', '');
 
-      presonalMovieDB.movies[a] = b;
-      presonalMovieDB.movies[c] = d;
+//       presonalMovieDB.movies[a] = b;
+//       presonalMovieDB.movies[c] = d;
 
-      console.log(presonalMovieDB);
+//       console.log(presonalMovieDB);
+
+// const bek = 50;
+// (bek === 50) ? console.log('Ok') : console.log('Ne verno'); 
+
+
+/* const num = 50;
+switch (num) {      //switch делает строгое сравнение
+     case 49:
+     console.log('Neverno');
+     break;     // break нужен для оставки. Если ответ верный, без break
+                // он продолжит прыгать по case
+     case 55:
+     console.log('Neverno');
+     break;
+     case 50:
+     console.log('Verno');
+     break;
+     default:  //default нужен для того, если не будет ответа верного
+            console.log('Neverno');
+            break;
+} */
+
+// условия да
